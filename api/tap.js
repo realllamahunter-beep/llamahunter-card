da3c72609d3821e33c6dd5c371ea26fb80a3ad3e
realllamahunter-beep <realllamahunter@gmail.com>
2026-05-04 19:08:00 -0400

    debug: echo tap data

--- api/tap.js
+++ api/tap.js
@@ -1,40 +1,14 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const { calculateCmacData, decryptPicc } = require('node-sdm');
-
-const keyHex = (process.env.NTAG_KEY || '').trim();
-
-export default async function handler(req, res) {
-  // Handle duplicate params (trial watermark)
-  const rawParams = new URLSearchParams(req.url.split('?')[1] || '');
-  const allPiccData = rawParams.getAll('picc_data');
-  const allCmac = rawParams.getAll('cmac');
-  const picc_data = allPiccData[allPiccData.length - 1];
-  const cmac = allCmac[allCmac.length - 1];
-
-  if (!picc_data || !cmac) {
-    return res.redirect('/?valid=false');
-  }
-
-  try {
-    const piccBuffer = Buffer.from(picc_data, 'hex');
-    const cmacBuffer = Buffer.from(cmac, 'hex');
-    const keyBuffer = Buffer.from(keyHex, 'hex');
-
-    // Decrypt first, then verify CMAC over plain data
-    const decrypted = decryptPicc(piccBuffer, keyBuffer);
-    const uid = decrypted.uid;
-    const counter = decrypted.cntInt;
-
-    const expectedCmac = calculateCmacData(uid, counter, keyBuffer);
-
-    if (expectedCmac.equals(cmacBuffer)) {
-      return res.redirect(`/?uid=${uid}&counter=${counter}&valid=true`);
-    } else {
-      return res.redirect('/?valid=false');
-    }
-  } catch (e) {
-    console.error(e);
-    return res.redirect('/?valid=false');
-  }
+export default function handler(req, res) {
+  const query = req.url.split('?')[1] || '';
+  const params = Object.fromEntries(
+    new URLSearchParams(query).entries()
+  );
+  res.status(200).json({
+    fullQuery: query,
+    paramNames: Object.keys(params),
+    piccDataLength: (params.picc_data || '').length,
+    cmacLength: (params.cmac || '').length,
+    piccDataValue: (params.picc_data || '').substring(0, 20) + '...',
+    cmacValue: (params.cmac || '').substring(0, 10) + '...',
+  });
 }
